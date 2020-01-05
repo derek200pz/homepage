e9dfba8960144f8332443acb7e76848664c52e25
derek200pz <derek200pp@gmail.com>
2020-01-05 16:39:28 -0600

    modified images, added content, supported mobile

--- static-site/js/home-controller.js
+++ static-site/js/home-controller.js
@@ -5,8 +5,47 @@ var verb = function(obj) {
 var rand = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+//check if an element is in the viewport
+function isElementInViewport(elem) {
+    var narrowing = 100;
+    var $elem = $(elem);
+    verb($elem);
+    // Get the scroll position of the page.
+    //var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $('html').scrollTop() + narrowing;
+    var viewportBottom = viewportTop + $(window).height() - 2 * narrowing;
+
+    // Get the position of the element on the page.
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+    //verb("viewportTop: " + viewportTop + "viewportBottom: " + viewportBottom + "elemTop: " + elemTop + "elemBottom: " + elemBottom);
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// If an element is in view, add the start class to it.
+function checkAnimation() {
+    var $elem = $('h1');
+    $elem.each(function() {
+        if ($(this).hasClass('slide')) return;
+        if (isElementInViewport($(this))) {
+            $(this).addClass('slide');
+        }
+    });
+}
+
 $(document).ready(
     function() {
+
+        //color all the H1 elements white to hide them before their animations
+        $('h1').addClass("white-text");
+
+        //----------CODE TO RUN ANIMATIONS ONLY WHEN THEY COME INTO VIEW----------
+        $(window).scroll(function() {
+            checkAnimation();
+        });
+
+        //----------CODE TO RETRIEVE AND INSERT CONTENT FROM JSON RANDOMLY----------
         var json = null;
         $.ajax({
             'global': false,
@@ -19,6 +58,5 @@ $(document).ready(
                 $("#home-currently").html(data.homeCurrently)
             }
         });
-
     }
 )
